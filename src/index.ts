89568a8dcf3f101f5e66c3506485b014516f3bdf
owain <owain@wegiveventures.com>
2020-05-23 14:22:10 +0100

    extracted login to module

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
 import * as puppeteer from 'puppeteer'
 // not sure why we are starting
 // before
-
-const LOGIN_SELECTOR = '#btn-passport-login'
-const LOGIN_PAGE = 'https://denham.hub.clubv1.com/'
+import loginDomain from './modules/login'
 
 ;(async () => {
 
   try {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    await page.goto(LOGIN_PAGE)
-    console.log('Got page')
-    const loginWindowPromise = new Promise((x) => page.once('popup', x))
-    await page.click(LOGIN_SELECTOR)
-    const loginWindow: any = await loginWindowPromise
-    // @ts-ignore
-    await loginWindow.click('#Username')
-    await loginWindow.keyboard.type(process.env.EMAIL)
-    await loginWindow.click('#Password')
-    await loginWindow.keyboard.type(process.env.PASSWORD)
-    await loginWindow.click('#RememberMe')
-    await loginWindow.click('#form0 > div:nth-child(8) > input')
+    await loginDomain.loginUser(page)
     console.log('Closing browser')
     await browser.close()
   } catch (error) {
---
+++ src/modules/login.ts
@@ -0,0 +1,27 @@
+const loginDomain = {
+  LOGIN_SELECTOR: '#btn-passport-login',
+  LOGIN_PAGE: 'https://denham.hub.clubv1.com/',
+
+  loginUser: async (page: any) => {
+    console.log('Start LoginUser')
+    try {
+      await page.goto(loginDomain.LOGIN_PAGE)
+      console.log('Got page')
+      const loginWindowPromise = new Promise((x) => page.once('popup', x))
+      await page.click(loginDomain.LOGIN_SELECTOR)
+      const loginWindow: any = await loginWindowPromise
+      // @ts-ignore
+      await loginWindow.click('#Username')
+      await loginWindow.keyboard.type(process.env.EMAIL)
+      await loginWindow.click('#Password')
+      await loginWindow.keyboard.type(process.env.PASSWORD)
+      await loginWindow.click('#RememberMe')
+      await loginWindow.click('#form0 > div:nth-child(8) > input')
+      console.log('Completed User Login')
+    } catch (loginError) {
+      console.log('Failed to log in')
+    }
+  }
+}
+
+export default loginDomain
